79ac663077450b8f64272381bcf86696a181bc5a
sampley <sampley@ualberta.ca>
2018-08-18 14:13:48 -0600

    Added problem 6

---
+++ src/p6/mod.rs
@@ -0,0 +1,16 @@
+pub fn run() {
+	println!("{}", difference(1, 100));
+}
+
+fn difference(min: u64, max: u64) -> u64 {
+	square_of_sum(min, max) - sum_of_squares(min, max)
+}
+
+fn sum_of_squares(min: u64, max: u64) -> u64 {
+	(min..max+1).map(|x| x * x).sum()
+}
+
+fn square_of_sum(min: u64, max: u64) -> u64 {
+	let x: u64 = (min..max+1).sum();
+	x * x
+}
