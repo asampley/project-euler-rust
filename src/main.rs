ec2b91e4aee0a28d21f0bcf89683ac6079b66ed2
sampley <sampley@ualberta.ca>
2018-08-18 14:15:34 -0600

    Added problems 6, 7, and 8 to main

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,11 @@ mod prime;
 
 #[macro_use]
 mod mods;
-mods!(p1,p2,p3,p4,p5);
+mods!(p1,p2,p3,p4,p5,p6,p7,p8);
 
 #[macro_use]
 mod problems;
 
 fn main() {
-	print_problems!(p1,p2,p3,p4,p5);
+	print_problems!(p1,p2,p3,p4,p5,p6,p7,p8);
 }
