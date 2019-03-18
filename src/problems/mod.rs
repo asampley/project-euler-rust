ab6ec46bb3003a5aeb3cc3f7972c8dfefa64ca8f
sampley <sampley@ualberta.ca>
2019-03-18 17:00:08 -0600

    Added problem 14

--- src/problems/mod.rs
+++ src/problems/mod.rs
@@ -1,1 +1,1 @@
-pub_mods!(p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12);
+pub_mods!(p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p14);
---
+++ src/problems/p14.rs
@@ -0,0 +1,46 @@
+use std::collections::HashMap;
+use std::collections::hash_map::Entry;
+use std::cmp::max;
+
+pub fn run() {
+    let mut cache = CollatzCache::new();
+    println!("{}", longest_chain_start(1_000_000 - 1, &mut cache));
+}
+
+struct CollatzCache {
+    cache: HashMap<u64, usize>
+}
+
+impl CollatzCache {
+    fn new() -> Self {
+        Self { cache: HashMap::new() }
+    }
+
+    fn collatz_count(&mut self, n: u64) -> usize {
+        match self.cache.get(&n) {
+            Some(&c) => c,
+            None => {
+                match n {
+                    0 | 1 => 1,
+                    n => {
+                        let next = 1 + self.collatz_count(CollatzCache::collatz_next(n));
+                        *self.cache.entry(n).or_insert(next)
+                    }
+                }
+            }
+        }
+    }
+
+    fn collatz_next(n: u64) -> u64 {
+        if n % 2 == 0 {
+            n / 2
+        } else {
+            3 * n + 1
+        }
+    }
+}
+
+fn longest_chain_start(max_start: u64, cache: &mut CollatzCache) -> u64 {
+    (1..=max_start)
+        .fold(0, |acc, x| if cache.collatz_count(acc) > cache.collatz_count(x) { acc } else { x })
+}
