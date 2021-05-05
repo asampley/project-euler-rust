40bf4912d7d2e83e48557b30addbe05181159446
Alex Sampley <sampley@ualberta.ca>
2021-05-05 14:51:43 +0000

    Added argument parsing so that you can run specific problems

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use std::env;
+use std::collections::HashSet;
+
 #[macro_use]
 mod macros;
 
@@ -5,11 +8,23 @@ mod numbers;
 mod problems;
 
 fn main() {
-    for (i, problem) in problems::all_problems() {
-        let start = std::time::Instant::now();
-        print!("{}: ", i);
-        problem();
-        let elapsed = start.elapsed();
-        println!("  Time taken: {}.{:06}s", elapsed.as_secs(), elapsed.subsec_micros());
+    let args: HashSet<usize> = env::args().skip(1).map(|s| s.parse().expect("Supplied argument not a number")).collect();
+
+    if args.is_empty() {
+        for (i, problem) in problems::all_problems() {
+            run(i, problem);
+        }
+    } else {
+        for (i, problem) in problems::all_problems().filter(|(i, _)| args.contains(i)) {
+            run(i, problem);
+        }
     }
+}
+
+fn run(i: usize, f: &fn()) {
+    let start = std::time::Instant::now();
+    print!("{}: ", i);
+    f();
+    let elapsed = start.elapsed();
+    println!("  Time taken: {}.{:06}s", elapsed.as_secs(), elapsed.subsec_micros());
 }
