383c9a17c9110ee63d0cbf49fb4e7bdfe5c47e0f
Alex Sampley <sampley@ualberta.ca>
2021-05-06 21:24:50 +0000

    Added problem 26

--- src/problems/mod.rs
+++ src/problems/mod.rs
@@ -1,32 +1,31 @@
-pub_mods!(p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16, p17, p19, p20, p21, p22, p23, p24, p25);
+pub_mods!(p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16, p17, p19, p20, p21, p22, p23, p24, p25, p26);
 
 pub fn all_problems() -> impl Iterator<Item = (usize, &'static fn())> {
-    let mut problems = Vec::new();
-
-    problems.push((1, &(p1::run as fn())));
-    problems.push((2, &(p2::run as fn())));
-    problems.push((3, &(p3::run as fn())));
-    problems.push((4, &(p4::run as fn())));
-    problems.push((5, &(p5::run as fn())));
-    problems.push((6, &(p6::run as fn())));
-    problems.push((7, &(p7::run as fn())));
-    problems.push((8, &(p8::run as fn())));
-    problems.push((9, &(p9::run as fn())));
-    problems.push((10, &(p10::run as fn())));
-    problems.push((11, &(p11::run as fn())));
-    problems.push((12, &(p12::run as fn())));
-    problems.push((13, &(p13::run as fn())));
-    problems.push((14, &(p14::run as fn())));
-    problems.push((15, &(p15::run as fn())));
-    problems.push((16, &(p16::run as fn())));
-    problems.push((17, &(p17::run as fn())));
-    problems.push((19, &(p19::run as fn())));
-    problems.push((20, &(p20::run as fn())));
-    problems.push((21, &(p21::run as fn())));
-    problems.push((22, &(p22::run as fn())));
-    problems.push((23, &(p23::run as fn())));
-    problems.push((24, &(p24::run as fn())));
-    problems.push((25, &(p25::run as fn())));
-
-    problems.into_iter()
+    vec![
+        (1, &(p1::run as fn())),
+        (2, &(p2::run as fn())),
+        (3, &(p3::run as fn())),
+        (4, &(p4::run as fn())),
+        (5, &(p5::run as fn())),
+        (6, &(p6::run as fn())),
+        (7, &(p7::run as fn())),
+        (8, &(p8::run as fn())),
+        (9, &(p9::run as fn())),
+        (10, &(p10::run as fn())),
+        (11, &(p11::run as fn())),
+        (12, &(p12::run as fn())),
+        (13, &(p13::run as fn())),
+        (14, &(p14::run as fn())),
+        (15, &(p15::run as fn())),
+        (16, &(p16::run as fn())),
+        (17, &(p17::run as fn())),
+        (19, &(p19::run as fn())),
+        (20, &(p20::run as fn())),
+        (21, &(p21::run as fn())),
+        (22, &(p22::run as fn())),
+        (23, &(p23::run as fn())),
+        (24, &(p24::run as fn())),
+        (25, &(p25::run as fn())),
+        (26, &(p26::run as fn())),
+    ].into_iter()
 }
---
+++ src/problems/p26.rs
@@ -0,0 +1,27 @@
+//! Each repeating decimal can be rewritten as as something over 10^n - 1 (several 9s).
+//! The exception seems to be if the denominator shares any factors with 10.
+//!
+//! It would take more mathematics to prove this is correct, but it looks like if all
+//! 2s and 5s are removed from the denominator, the lenght of the repetition can be
+//! determined by iterating through all powers of 10 minus 1.
+
+use num::{ BigUint, One, Zero };
+
+pub fn run() {
+    println!("{}", (1..1000).map(|x| (cycle_length(x), x)).max().map(|(_, x)| x).unwrap());
+}
+
+fn cycle_length(mut n: u64) -> u32 {
+    while n % 2 == 0 {
+        n /= 2;
+    }
+
+    while n % 5 == 0 {
+        n /= 5;
+    }
+
+    (1..).map(|i| (i, BigUint::from(10_u64).pow(i) - BigUint::one()))
+        .find(|(_, p)| p % n == BigUint::zero())
+        .map(|(i, _)| i)
+        .unwrap()
+}
