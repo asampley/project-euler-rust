382a3129c5c386b6f8ea0de3a3d5fde51dee86e1
Alex Sampley <sampley@ualberta.ca>
2021-05-06 20:08:13 +0000

    Made fibonacci more generic, and completed problem 25

--- src/numbers/fibonacci.rs
+++ src/numbers/fibonacci.rs
@@ -1,6 +1,8 @@
-pub struct FibIter {
-	f0: u64,
-	f1: u64,
+use std::ops::AddAssign;
+
+pub struct FibIter<T> {
+	f0: T,
+	f1: T,
 	state: IterState,
 }
 
@@ -10,8 +12,8 @@ enum IterState {
 	Next,
 }
 
-impl FibIter {
-	pub fn new(f0: u64, f1: u64) -> FibIter {
+impl<T> FibIter<T> {
+	pub fn new(f0: T, f1: T) -> Self {
 		FibIter {
 			f0,
 			f1,
@@ -20,23 +22,27 @@ impl FibIter {
 	}
 }
 
-impl Iterator for FibIter {
-	type Item = u64;
-	fn next(&mut self) -> Option<u64> {
+impl<T> Iterator for FibIter<T>
+where
+    T: Clone,
+    for<'a> T: AddAssign<&'a T>,
+{
+	type Item = T;
+
+	fn next(&mut self) -> Option<Self::Item> {
 		Some(match self.state {
 			IterState::First => {
 				self.state = IterState::Second;
-				self.f0
+				self.f0.clone()
 			}
 			IterState::Second => {
 				self.state = IterState::Next;
-				self.f1
+				self.f1.clone()
 			}
 			IterState::Next => {
-				let temp = self.f1;
-				self.f1 = self.f0 + self.f1;
-				self.f0 = temp;
-				self.f1
+                std::mem::swap(&mut self.f1, &mut self.f0);
+                self.f1 += &self.f0;
+				self.f1.clone()
 			}
 		})
 	}
---
+++ src/problems/p25.rs
@@ -0,0 +1,13 @@
+use crate::numbers::fibonacci::FibIter;
+
+use num::{ BigUint, one };
+
+pub fn run() {
+    let one_thousand_digits = BigUint::from(10_u8).pow(999);
+
+    println!("{}", FibIter::<BigUint>::new(one(), one())
+        .enumerate()
+        .find(|(_, x)| x >= &one_thousand_digits)
+        .map(|(i, _)| i).unwrap() + 1
+    );
+}
